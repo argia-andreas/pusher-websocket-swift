f3954114958ed8116a4ed153e540f52ebd1f41f3
AndreasG <andreas@argia.se>
2020-08-11 11:55:14 +0200

    Bumped version

--- Sources/MyAuthRequestBuilder.swift
+++ Sources/MyAuthRequestBuilder.swift
@@ -2,7 +2,7 @@
     func requestFor(socketID: String, channelName: String) -> URLRequest? {
         var request = URLRequest(url: URL(string: "http://localhost:9292/builder")!)
         request.httpMethod = "POST"
-        request.httpBody = "socket_id=\(socketID)&channel_name=\(channel.name)".data(using: String.Encoding.utf8)
+        request.httpBody = "socket_id=\(socketID)&channel_name=\(channelName)".data(using: String.Encoding.utf8)
         request.addValue("myToken", forHTTPHeaderField: "Authorization")
         return request
     }
