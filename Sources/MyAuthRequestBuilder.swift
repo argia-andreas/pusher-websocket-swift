510730b7cdf8932bcd6d3955875ca9abd484d714
AndreasG <andreas@argia.se>
2020-08-11 12:13:23 +0200

    Fix.

--- Sources/MyAuthRequestBuilder.swift
+++ Sources/MyAuthRequestBuilder.swift
@@ -1,4 +1,4 @@
-@objc(MyAuthRequestBuilder) open class MyAuthRequestBuilder: AuthRequestBuilderProtocol {
+open class MyAuthRequestBuilder: AuthRequestBuilderProtocol {
     func requestFor(socketID: String, channelName: String) -> URLRequest? {
         var request = URLRequest(url: URL(string: "http://localhost:9292/builder")!)
         request.httpMethod = "POST"
